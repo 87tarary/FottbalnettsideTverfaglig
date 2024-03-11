2dd55f07a32a5a054a05f97f9edb79ef43a2512d
“87tarary” <87tarary@stud.akademiet.no>
2024-03-11 13:37:08 +0100

    test

--- src/test/java/com/example/fottbalnettsidetverfaglig/FottbalnettsideTverfagligApplicationTests.java
+++ src/test/java/com/example/fottbalnettsidetverfaglig/FottbalnettsideTverfagligApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FottbalnettsideTverfagligApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
