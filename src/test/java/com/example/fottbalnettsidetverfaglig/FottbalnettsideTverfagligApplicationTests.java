b622a989dbad75c559c4a0df666e561d287112dc
“87tarary” <87tarary@stud.akademiet.no>
2024-03-11 13:35:16 +0100

    Initial commit

---
+++ src/test/java/com/example/fottbalnettsidetverfaglig/FottbalnettsideTverfagligApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fottbalnettsidetverfaglig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FottbalnettsideTverfagligApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
